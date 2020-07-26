48560f247cce353e73a69007fa603e1448b728d0
Tais Antunes <tais.antunes@compasso.com.br>
2020-07-26 19:04:00 -0300

    primeiro commit

---
+++ src/main/java/br/com/api/cadastro/cliente/apicadastrocliente/ApiCadastroClienteApplication.java
@@ -0,0 +1,13 @@
+package br.com.api.cadastro.cliente.apicadastrocliente;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCadastroClienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCadastroClienteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/api/cadastro/cliente/apicadastrocliente/ApiCadastroClienteApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.api.cadastro.cliente.apicadastrocliente;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiCadastroClienteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
